b1e307ab22b1dd735bd9e0065bd747363f5172e6
wernermendezz <wernermendezz@users.noreply.github.com>
2019-06-06 17:35:24 -0600

    actualizacion de codigo

--- pruebaEnVisualGit/MP3/MP3/Program.cs
+++ pruebaEnVisualGit/MP3/MP3/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace pruebaEnVisualGit
+namespace MP3
 {
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
